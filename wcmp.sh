6b7398a381ce76d3fdc8fe68cce3e42a3923a0ab
Daniel Madridano <8.ddedaniel@gmail.com>
2025-11-30 16:35:35 +0100

    Funciones Opcion-Menu

--- wcmp.sh
+++ wcmp.sh
@@ -1,1 +1,33 @@
 #!/bin/bash
+
+Opcio-Menu() {
+  echo "---------------------------------------------------------"
+  echo "Where Cars Meet Passion (Base de Dades)"
+  echo "---------------------------------------------------------"
+  echo "1. La nostra selecció de vehicles."
+  echo "2. Troba el teu cotxe."
+  echo "3. Demostra que ets un expert."
+  echo "4. Sistema gestor de base de dades."
+  echo "0. Sortir."
+  echo "Selecciona una opció:"
+}
+
+enDesenvolupament() {
+  clear
+  echo "En desenvolupament"
+  read -n 1 -s
+  clear
+}
+
+opcioNoValida() {
+  clear
+  echo "Error: Opció no vàlida"
+  sleep 2
+  clear
+}
+
+Sortida() {
+  clear
+  echo "Gràcies per visitar Where Cars Meet Pasion"
+  exit
+}
